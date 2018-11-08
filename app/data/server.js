9d46e8c0fff40dfb179696fd55023b8c41155ade
David Applesauce <dsweck@gmail.com>
2018-11-07 22:13:20 -0500

    friendfinder

--- app/data/server.js
+++ app/data/server.js
@@ -1,5 +1,5 @@
 var express = require("express");
-
+var path = require("path");
 var app = express();
 
 var PORT = process.env.PORT || 8080;
@@ -8,9 +8,12 @@ var PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-//Routes
+
 // require("./routes/apiRoutes")(app);
 // require("./routes/htmlRoutes")(app);
+
+require(path.join(__dirname, './app/routing/apiRoutes'))(app);
+require(path.join(__dirname, './app/routing/htmlRoutes'))(app);
 
 //Listener
 app.listen(PORT, function() {
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,8 +1,48 @@
-var friendData = require("../data/friends");
+var friendData = require("../data/friends.js");
+
+var path = require("path");
 
 module.exports = function(app) {
-
-    app.get("/api/survey", function(req, res) {
-        res.json(tableData);
+        
+    app.post("/api/friends", function(req, res) {
+        res.json(friends);
     });
-}
+
+
+	// Add new friend entry
+	app.post("/api/friends", function(req, res) {
+		
+		var userInput = req.body;
+		
+		var userResponses = userInput.scores;
+
+		// Compute best friend match
+		var matchName = "";
+		var totalDifference = 1000; 
+
+		// Examine all existing friends in the list
+		for (var i = 0; i < friends.length; i++) {
+			// console.log('friend = ' + JSON.stringify(friends[i]));
+
+			// Compute differenes for each question
+			var diff = 0;
+			for (var j = 0; j < userResponses.length; j++) {
+				diff += Math.abs(friends[i].scores[j] - userResponses[j]);
+			}
+			// console.log('diff = ' + diff);
+
+			// If lowest difference, record the friend match
+			if (diff < totalDifference) {
+
+				totalDifference = diff;
+				matchName = friends[i].name;
+			}
+		}
+
+		// Add new user
+		friends.push(userInput);
+
+		// Send appropriate response
+		res.json({status: "OK", matchName: matchName});
+	});
+};
